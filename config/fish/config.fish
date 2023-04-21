4c0d30b9efa64fbf2c479dc085d2dacf38de156b
alipnf <alifnur1224@gmail.com>
2023-04-21 11:57:54 +0700

    feat: add alias git

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    alias g="git"
+    alias gst="git status"
+    alias gci="git commit"
+    alias gcim="git commit -am"
+    alias gco="git chekout"
+    alias gbr="git branch"
 end
+
+
