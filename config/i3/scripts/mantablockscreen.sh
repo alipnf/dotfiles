3cc8f1cf9c889d597a080e03311476218a2b3baf
alipnf <alifnur1224@gmail.com>
2023-07-11 14:57:25 +0700

    feat(i3/script): add mantablockscree.sh

---
+++ config/i3/scripts/mantablockscreen.sh
@@ -0,0 +1,30 @@
+!/usr/bin/env bash
+
+PICTURE=/tmp/i3lock.png
+SCREENSHOT="scrot -z $PICTURE"
+
+BLUR="5x4"
+
+$SCREENSHOT
+convert $PICTURE -blur $BLUR $PICTURE
+
+fg_color=fefefeff
+wrong_color=f82a11aa
+highlight_color=39393999
+verif_color=fefefe66
+
+i3lock -n --force-clock -i $PICTURE --ind-pos="w/2:h/2" --time-pos="w/2:h/2-5" --date-pos="w/2:h/2+35" --greeter-pos="w/2:h/2" \
+	--insidever-color=5f5f5f66 --insidewrong-color=$wrong_color --inside-color=5f5f5f66 \
+	--ringver-color=$verif_color --ringwrong-color=$wrong_color --ring-color=$fg_color \
+	--keyhl-color=$highlight_color --bshl-color=$highlight_color --separator-color=00000000 \
+	--date-color=$fg_color --time-color=$fg_color --greeter-color=$fg_color \
+	--time-str="%H | %M" --time-size=40 \
+	--date-str="%a, %d %b" --date-size=25 \
+	--greeter-text="$date_now" --greeter-size=25 \
+	--line-uses-inside --radius 75 --ring-width 2 --indicator \
+	--verif-text="" --wrong-text="" --noinput-text="" \
+	--clock --date-font="Abel" --time-font="Abel"
+
+rm $PICTURE
+
+!/bin/env bash
