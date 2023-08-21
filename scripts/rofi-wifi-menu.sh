3f08bc8931c02ba24d8befab69c09ce8a48aa1b2
alipnf <alifnur1224@gmail.com>
2023-08-21 17:37:17 +0700

    feat(rofi-wifi-menu): change theme

--- scripts/rofi-wifi-menu.sh
+++ scripts/rofi-wifi-menu.sh
@@ -31,7 +31,7 @@ else
 		nmcli connection up id "$chosen_id" | grep "successfully" && notify-send "Connection Established" "$success_message"
 	else
 		if [[ "$chosen_network" =~ "" ]]; then
-			wifi_password=$(rofi -dmenu -p "Password: ")
+			wifi_password=$(rofi -theme ~/.config/rofi/password.rasi -dmenu -p "Password: ")
 		fi
 		nmcli device wifi connect "$chosen_id" password "$wifi_password" | grep "successfully" && notify-send "Connection Established" "$success_message"
 	fi
