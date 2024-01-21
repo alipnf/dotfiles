bf92a3af5e16c35c134a26f3bfc4b5393a1900c1
alipnf <alipnf@proton.me>
2024-01-21 15:03:03 +0700

    feat(nvim) add mason.lua

---
+++ config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,11 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "eslint-lsp",
+      "prettierd",
+      "tailwindcss-language-server",
+      "typescript-language-server",
+    },
+  },
+}
