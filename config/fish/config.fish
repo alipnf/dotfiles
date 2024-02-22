8d18fc0cb68e757eabaeb1a272353be83f365114
alipnf <alipnf@proton.me>
2024-02-22 21:20:59 +0700

    feat(fish): add global setting

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,24 +1,26 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    alias g="git"
-    alias gf="git fetch"
-    alias gp="git pull"
-    alias ga="git add"
-    alias gst="git status"
-    alias gci="git commit -m"
-    alias gcim="git commit -am"
-    alias gco="git checkout"
-    alias gbr="git branch"
-    alias gps="git push"
-    alias code="codium"
-    alias ll="exa --long --header -a"
-    alias bl="sudo systemctl enable bluetooth.service"
-    alias wlan="networkmanager_dmenu"
-    alias v="nvim"
-    alias sxs="sudo xampp start"
-    alias sp="spotifyctl"
-    alias pkglist="pacman -Qqe > package_list.txt"
-    alias t="tmux"
-end
+#global
+set -gx EDITOR nvim
+set fish_greeting ""
+
+# alias
+alias g="git"
+alias gf="git fetch"
+alias gp="git pull"
+alias ga="git add"
+alias gst="git status"
+alias gci="git commit -m"
+alias gcim="git commit -am"
+alias gco="git checkout"
+alias gbr="git branch"
+alias gps="git push"
+alias code="codium"
+alias ll="exa --long --header -a"
+alias bl="sudo systemctl enable bluetooth.service"
+alias wlan="networkmanager_dmenu"
+alias v="nvim"
+alias sxs="sudo xampp start"
+alias sp="spotifyctl"
+alias pkglist="pacman -Qqe > package_list.txt"
+alias t="tmux"
 
 starship init fish | source
