7f4536f3a99566ba19c64f5321fa9ba158ccfb55
alipnf <alipnf@proton.me>
2024-04-28 23:47:39 +0700

    feat(nvim): migrate live-server to pnpm

--- config/nvim/lua/plugins/live-server.lua
+++ config/nvim/lua/plugins/live-server.lua
@@ -1,5 +1,7 @@
-return {
-  "barrett-ruth/live-server.nvim",
-  build = "yarn global add live-server",
-  config = true,
+return
+{
+  'barrett-ruth/live-server.nvim',
+  build = 'pnpm add -g live-server',
+  cmd = { 'LiveServerStart', 'LiveServerStop' },
+  config = true
 }
