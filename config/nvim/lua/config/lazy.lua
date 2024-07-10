cfa8151c8388b3e831c81dbb7f7778ef6791b2fd
alipnf <alipnf@proton.me>
2024-07-10 21:52:26 +0700

    feat(nvim): add extras mini-surround

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
 
     { import = "plugins" },
   },
