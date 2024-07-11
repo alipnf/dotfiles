5cac965a2a74ce774f7940a6bcafcc4ea2a10ba8
alipnf <alipnf@proton.me>
2024-07-11 19:17:18 +0700

    feat(nvim): disable extra lang ts & java

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -18,10 +18,10 @@ require("lazy").setup({
     -- formating
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- lang
-    { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     -- linting
     { import = "lazyvim.plugins.extras.linting.eslint" },
