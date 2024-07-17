7ea712968619ff26fac4d1933f74b0164fc61fa4
alipnf <alipnf@proton.me>
2024-07-17 17:08:57 +0700

    feat(nvim): add lspconfig.lua

---
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,16 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = function()
+    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    keys[#keys + 1] = {
+      "<leader>cr",
+      function()
+        local inc_rename = require("inc_rename")
+        return ":" .. inc_rename.config.cmd_name .. " " .. vim.fn.expand("<cword>")
+      end,
+      expr = true,
+      desc = "Rename (inc-rename.nvim)",
+      has = "rename",
+    }
+  end,
+}
