f274e38a0d019e74df29c44e9918dddf64e6be1f
alipnf <alipnf@proton.me>
2024-08-08 20:05:22 +0700

    feat(nvim): add keymaps icon-picker

---
+++ config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+-- icon-picker
+map("n", "<Leader>ic", "<cmd>IconPickerNormal<cr>", { noremap = true, silent = true })
+map("n", "<Leader>iy", "<cmd>IconPickerYank<cr>", { noremap = true, silent = true })
+map("i", "<C-c>", "<cmd>IconPickerInsert<cr>", { noremap = true, silent = true })
