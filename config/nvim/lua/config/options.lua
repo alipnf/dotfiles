ef366bf8ed19f9a99e119c67f23f4df658d2cff5
alipnf <alipnf@proton.me>
2025-01-25 14:41:19 +0700

    chore(options): set utf-8 encoding and disable backups

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,5 +1,9 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
 vim.opt.relativenumber = false
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.backup = false
+vim.opt.path:append({ "**" })
+vim.opt.wildignore:append({ "*/node_modules/*" })
