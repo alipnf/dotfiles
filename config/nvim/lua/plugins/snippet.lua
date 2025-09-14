26f9d303af0df1251c1fff1745f8c8dcf8355147
alipnf <alipnf@proton.me>
2025-09-14 17:19:46 +0700

    fix(snippet): enable close tag autocompletion
    Fixes issue where close tag autocompletion was disabled.
    Enables `enable_close` option in `nvim-ts-autotag`.

--- config/nvim/lua/plugins/snippet.lua
+++ config/nvim/lua/plugins/snippet.lua
@@ -1,52 +1,52 @@
 return {
-    {
-        "L3MON4D3/LuaSnip",
-        lazy = true,
-        build = (not LazyVim.is_win())
-                and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
-            or nil,
-        dependencies = {
-            {
-                "rafamadriz/friendly-snippets",
-                config = function()
-                    require("luasnip.loaders.from_vscode").lazy_load()
+  {
+    "L3MON4D3/LuaSnip",
+    lazy = true,
+    build = (not LazyVim.is_win())
+        and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
+      or nil,
+    dependencies = {
+      {
+        "rafamadriz/friendly-snippets",
+        config = function()
+          require("luasnip.loaders.from_vscode").lazy_load()
 
-                    -- Extend HTML snippets to work in JSX, TSX, Astro
-                    local ft_extend = require("luasnip").filetype_extend
-                    ft_extend("javascriptreact", { "html" })
-                    ft_extend("typescriptreact", { "html" })
-                    ft_extend("astro", { "html" })
-                end,
-            },
-        },
-        opts = {
-            history = true,
-            delete_check_events = "TextChanged",
-        },
-    },
-
-    {
-        "windwp/nvim-ts-autotag",
-        event = { "BufReadPre", "BufNewFile" },
-        opts = {
-            opts = {
-                enable_close = false,
-                enable_rename = true,
-                enable_close_on_slash = false,
-            },
-        },
-    },
-
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = function(_, opts)
-            vim.list_extend(opts.ensure_installed, {
-                "astro",
-                "html",
-                "tsx",
-                "javascript",
-                "typescript",
-            })
+          -- Extend HTML snippets to work in JSX, TSX, Astro
+          local ft_extend = require("luasnip").filetype_extend
+          ft_extend("javascriptreact", { "html" })
+          ft_extend("typescriptreact", { "html" })
+          ft_extend("astro", { "html" })
         end,
+      },
     },
+    opts = {
+      history = true,
+      delete_check_events = "TextChanged",
+    },
+  },
+
+  {
+    "windwp/nvim-ts-autotag",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      opts = {
+        enable_close = true,
+        enable_rename = true,
+        enable_close_on_slash = false,
+      },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "astro",
+        "html",
+        "tsx",
+        "javascript",
+        "typescript",
+      })
+    end,
+  },
 }
