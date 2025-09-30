4266dadf63871fba185e51eef3ccabbacabfbd96
alipnf <alipnf@proton.me>
2025-09-30 17:01:23 +0700

    feat(nvim): update cord.lua

--- config/nvim/lua/plugins/cord.lua
+++ config/nvim/lua/plugins/cord.lua
@@ -1,21 +1,21 @@
 return {
-    "vyfor/cord.nvim",
-    event = "VeryLazy",
-    enabled = true,
-    opts = {
-        editor = {
-            -- client = "1182300039712870430", -- Custom Discord Application ID untuk Valorant
-            tooltip = "Lazyvim",
-        },
-        buttons = {
-            {
-                label = function(opts)
-                    return opts.repo_url and "View Repository" or "View Profile"
-                end,
-                url = function(opts)
-                    return opts.repo_url or "https://github.com/alipnf"
-                end,
-            },
-        },
+  "vyfor/cord.nvim",
+  event = "VeryLazy",
+  enabled = true,
+  opts = {
+    editor = {
+      -- client = "1182300039712870430", -- Custom Discord Application ID untuk Valorant
+      tooltip = "Lazyvim",
     },
+    buttons = {
+      {
+        label = function(opts)
+          return opts.repo_url and "View Repository" or "View Profile"
+        end,
+        url = function(opts)
+          return opts.repo_url or "https://github.com/alipnf"
+        end,
+      },
+    },
+  },
 }
