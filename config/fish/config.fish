a2d51757d768e457ecffefbb8eee60fdb98fdab6
alipnf <alipnf@proton.me>
2026-03-28 12:08:18 -0400

    feat(fish): update config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,8 +1,8 @@
-#global
+# global
 set -gx EDITOR nvim
 set fish_greeting ""
 
-# alias
+# git
 alias g="git"
 alias gf="git fetch"
 alias gp="git pull"
@@ -13,21 +13,35 @@ alias gcim="git commit -am"
 alias gco="git checkout"
 alias gbr="git branch"
 alias gps="git push"
+
+# editor
+alias v="nvim"
 alias code="codium"
+
+# file
 alias ld="eza -lD"
 alias lh="eza -dl .* --group-directories-first"
 alias ll="eza -al --group-directories-first"
 alias ls="eza -alf --color=always --sort=size | grep -v /"
 alias lt="eza -al --sort=modified"
-alias bl="sudo systemctl enable bluetooth.service"
-alias wlan="~/.scripts/rofi-wifi-menu.sh"
-alias v="nvim"
-alias sxs="sudo xampp start"
-alias sp="spotifyctl"
-alias pkglist="pacman -Qqe > package_list.txt"
-alias t="tmux"
 alias yz="yazi"
 
+# terminal
+alias t="tmux"
+
+# system
+alias bl="sudo systemctl enable bluetooth.service"
+alias pkglist="pacman -Qqen > pkglist-official.txt && pacman -Qqem > pkglist-aur.txt"
+# restore official: sudo pacman -S - < pkglist-official.txt
+# restore aur:      yay -S - < pkglist-aur.txt
+
+# network
+alias wlan="~/.scripts/rofi-wifi-menu.sh"
+
+# apps
+alias sp="spotifyctl"
+
+# shell
 starship init fish | source
 zoxide init fish | source
 
@@ -40,4 +54,3 @@ set -gx PNPM_HOME "/home/alipnf/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
 end
-# pnpm end
